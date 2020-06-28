dbabda3c64f39a750745116a66394989fb314346
Jef <278971585@qq.com>
2020-06-28 14:17:07 +0800

    加入websocket 动态添加dom

---
+++ src/main/java/com/ljf/ruleproject/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.ljf.ruleproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by mr.lin on 2020/6/28
+ * 开启WebSocket支持
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
