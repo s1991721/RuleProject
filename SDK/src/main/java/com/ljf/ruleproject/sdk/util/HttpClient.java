91e07b888d0652b6cd972e13ce3a12756330daff
Jef <278971585@qq.com>
2020-07-03 17:27:54 +0800

    封装http工具

--- SDK/src/main/java/com/ljf/ruleproject/sdk/util/HttpClient.java
+++ SDK/src/main/java/com/ljf/ruleproject/sdk/util/HttpClient.java
@@ -1,7 +1,23 @@
 package com.ljf.ruleproject.sdk.util;
 
+import okhttp3.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by mr.lin on 2020/7/3
  */
 public class HttpClient {
+
+    public static final int ConnectTimeOut = 10;//网络超时时间
+    public static final int ReadTimeOut = 30;
+    public static final int WriteTimeOut = 60;
+
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(ConnectTimeOut, TimeUnit.SECONDS)
+            .readTimeout(ReadTimeOut, TimeUnit.SECONDS)
+            .writeTimeout(WriteTimeOut, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build();
+
 }
