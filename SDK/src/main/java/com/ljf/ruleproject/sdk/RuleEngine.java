094be65dc950552d31b4aad614c40b9e4d4429f0
Jef <278971585@qq.com>
2020-07-03 17:58:08 +0800

    梳理SDK对外暴露的功能

---
+++ SDK/src/main/java/com/ljf/ruleproject/sdk/RuleEngine.java
@@ -0,0 +1,81 @@
+package com.ljf.ruleproject.sdk;
+
+/**
+ * Created by mr.lin on 2020/7/3
+ */
+public class RuleEngine {
+
+    //执行业务
+    public static void executeBusiness() {
+
+    }
+
+    //创建业务
+    public static void createBusiness() {
+
+    }
+    //删除业务
+    public static void deleteBusiness() {
+
+    }
+    //修改业务
+    public static void updateBusiness() {
+
+    }
+    //查找业务
+    public static void searchBusiness() {
+
+    }
+
+    //创建规则
+    public static void createRule() {
+
+    }
+    //删除规则
+    public static void deleteRule() {
+
+    }
+    //修改规则
+    public static void updateRule() {
+
+    }
+    //查询规则
+    public static void searchRule() {
+
+    }
+
+    //创建DBInfo
+    public static void createDBInfo() {
+
+    }
+    //删除DBInfo
+    public static void deleteDBInfo() {
+
+    }
+    //修改DBInfo
+    public static void updateDBInfo() {
+
+    }
+    //查询DBInfo
+    public static void searchDBInfo() {
+
+    }
+
+    //创建ClassInfo
+    public static void createClass() {
+
+    }
+    //删除ClassInfo
+    public static void deleteClass() {
+
+    }
+    //修改ClassInfo
+    public static void updateClass() {
+
+    }
+    //查询ClassInfo
+    public static void searchClass() {
+
+    }
+
+}
