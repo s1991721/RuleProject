00b81d1fe4e74ca6f5f03c1b956c0104c93270f5
Jef <278971585@qq.com>
2020-07-13 10:21:22 +0800

    舍弃封装DB的模式

--- Base/src/main/java/com/ljf/ruleproject/base/services/BaseService.java
+++
@@ -1,7 +0,0 @@
-package com.ljf.ruleproject.base.services;
-
-/**
- * Created by mr.lin on 2020/7/10
- */
-public class BaseService {
-}
--- Base/src/main/java/com/ljf/ruleproject/base/services/db/DBService.java
+++
@@ -1,7 +0,0 @@
-package com.ljf.ruleproject.base.services.db;
-
-/**
- * Created by mr.lin on 2020/7/10
- */
-public interface DBService {
-}
