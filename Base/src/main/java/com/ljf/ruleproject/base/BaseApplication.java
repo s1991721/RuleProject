d95285878739ad6b29237be1aab95b11a5c8251e
Jef <278971585@qq.com>
2020-07-17 09:46:47 +0800

    编写feignclient，完成上传manven私服的操作

--- Base/src/main/java/com/ljf/ruleproject/base/BaseApplication.java
+++ Base/src/main/java/com/ljf/ruleproject/base/BaseApplication.java
@@ -11,6 +11,7 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
 /**
  * Created by mr.lin on 2020/7/10
  */
+@EnableFeignClients
 @EnableCaching
 @EnableApolloConfig
 @SpringBootApplication
---
+++ Base/src/main/java/com/ljf/ruleproject/base/feign/MFeignClient.java
@@ -0,0 +1,15 @@
+package com.ljf.ruleproject.base.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by mr.lin on 2020/7/16
+ */
+@FeignClient
+public interface MFeignClient {
+
+    @GetMapping("/getCache")
+    String getCache();
+
+}
